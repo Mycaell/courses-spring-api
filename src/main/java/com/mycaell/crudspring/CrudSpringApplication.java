5365f5b17c73ebc15bed5150b6be7f45357a7912
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-26 01:40:14 -0300

    start values when running

--- src/main/java/com/mycaell/crudspring/CrudSpringApplication.java
+++ src/main/java/com/mycaell/crudspring/CrudSpringApplication.java
@@ -1,13 +1,33 @@
 package com.mycaell.crudspring;
 
+import com.mycaell.crudspring.domain.course.enums.Category;
+import com.mycaell.crudspring.domain.course.enums.Status;
+import com.mycaell.crudspring.domain.course.model.Course;
+import com.mycaell.crudspring.infrastructure.persistence.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course c = new Course();
+            c.setName("Angular com Spring");
+            c.setCategory(Category.FRONT_END);
+            c.setStatus(Status.ACTIVE);
+
+            courseRepository.save(c);
+        };
+    }
 }
