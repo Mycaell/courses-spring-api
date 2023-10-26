cf1b96c0cb2bbd6fab84f0f7027a91263404690a
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-26 00:50:34 -0300

    create course model

---
+++ src/main/java/com/mycaell/crudspring/domain/course/model/Course.java
@@ -0,0 +1,89 @@
+package com.mycaell.crudspring.domain.course.model;
+
+import com.mycaell.crudspring.domain.course.enums.Category;
+import com.mycaell.crudspring.domain.course.enums.Status;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Course implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id) && Objects.equals(name, course.name) && category == course.category && status == course.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", status=" + status +
+                '}';
+    }
+}
