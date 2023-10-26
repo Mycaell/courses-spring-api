af6685c904dda6d198b18b6be228e5d1fddaac4c
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-10-26 03:41:54 -0300

    adjust dtos record for class due to problems with model mapper

--- src/main/java/com/mycaell/crudspring/presentation/course/dto/CourseRequest.java
+++ src/main/java/com/mycaell/crudspring/presentation/course/dto/CourseRequest.java
@@ -8,8 +8,51 @@ import jakarta.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-public record CourseRequest(
-        @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull Category category,
-        @NotNull Status status) {
+import java.io.Serializable;
+
+public class CourseRequest implements Serializable {
+
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    private String name;
+
+    @NotNull
+    private Category category;
+
+    @NotNull
+    private Status status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseRequest{" +
+                "name='" + name + '\'' +
+                ", category=" + category +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/com/mycaell/crudspring/presentation/course/dto/CourseResponse.java
+++ src/main/java/com/mycaell/crudspring/presentation/course/dto/CourseResponse.java
@@ -3,10 +3,54 @@ package com.mycaell.crudspring.presentation.course.dto;
 import com.mycaell.crudspring.domain.course.enums.Category;
 import com.mycaell.crudspring.domain.course.enums.Status;
 
-public record CourseResponse(
-        Long id,
-        String name,
-        Category category,
-        Status status
-) {
+import java.io.Serializable;
+
+public class CourseResponse implements Serializable {
+
+    private Long id;
+    private String name;
+    private Category category;
+    private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", status=" + status +
+                '}';
+    }
 }
