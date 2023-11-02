e38aa30f8fea4934a71a169f1d9e7915010800a5
mycaell de oliveira <oliveiramycaell@gmail.com>
2023-11-02 12:51:38 -0300

    add log for errors

--- src/main/java/com/mycaell/crudspring/application/shared/exception/handler/ApplicationExceptionHandler.java
+++ src/main/java/com/mycaell/crudspring/application/shared/exception/handler/ApplicationExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.mycaell.crudspring.application.shared.exception.handler;
 
 import com.mycaell.crudspring.application.shared.exception.ResourceNotFoundException;
+
 import jakarta.validation.ConstraintViolationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,29 +14,34 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final MessageSource messageSource;
 
     public ApplicationExceptionHandler(MessageSource messageSource) {
         this.messageSource = messageSource;
+    }
+
+    public String getLogMessage(Exception exception, List<String> errorList) {
+        String title = "Ocorreu um erro:";
+        String type = exception.getClass().getSimpleName();
+        String errors = errorList.toString();
+        return String.format("%s %s %s", title, type, errors);
     }
 
     public String getMessage(String message) {
@@ -51,13 +60,11 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 
     public ResponseEntity<Object> handleException(Exception exception, HttpStatus status, WebRequest request, String key) {
         List<String> errors = List.of(getMessage(key));
-        return handleExceptionInternal(exception, errors, new HttpHeaders(), status, request);
-    }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        List<String> errors = getErrors(exception.getBindingResult());
-        return handleExceptionInternal(exception, errors, headers, HttpStatus.BAD_REQUEST, request);
+        String logMessage = getLogMessage(exception, errors);
+        logger.warn(logMessage, exception);
+
+        return handleExceptionInternal(exception, errors, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler({RuntimeException.class})
@@ -72,15 +79,23 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException exception, WebRequest request) {
-        List<String> errors = exception.getConstraintViolations().stream()
-                .map(e -> MessageFormat.format(e.getMessage(), e.getPropertyPath())).collect(Collectors.toList());
+        List<String> errors = exception.getConstraintViolations()
+                .stream()
+                .map(e -> MessageFormat.format(e.getMessage(), e.getPropertyPath()))
+                .collect(Collectors.toList());
 
         return handleExceptionInternal(exception, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler({ResourceNotFoundException.class})
-    public ResponseEntity<Object> handleDataIntegrityViolationException(ResourceNotFoundException exception, WebRequest request) {
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request) {
         return handleException(exception, HttpStatus.BAD_REQUEST, request, "resource.not-found");
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errors = getErrors(exception.getBindingResult());
+        return handleExceptionInternal(exception, errors, headers, HttpStatus.BAD_REQUEST, request);
     }
 
 }
